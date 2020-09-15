5f1389143a5e9de689318da1db805c77dd2949e7
min <minhazur9@gmail.com>
2020-09-15 18:59:19 -0400

    data types

---
+++ scripts/app.js
@@ -0,0 +1,60 @@
+// Light Switch would be a boolean
+// Email would be a string
+// Spaceship would be an object
+// List of student names would be an array of strings
+// Each having a location would be an array of objects 
+// Each having a list of shows would be an array of objects containing another array
+
+const data = {
+    switchOn: false,
+    email: "minhazur9@gmail.com",
+    spaceship: {
+        hull: true,
+        laserBlaster: true,
+        tractorBeam: true,
+        warpDrive: true
+    },
+    students: [
+        {
+            name: "Min",
+            location: "US",
+            favShows: [
+                "Dexter",
+                "Gotham",
+                "Brookyn Nine Nine"
+            ]
+        },
+
+        {
+            name: "William",
+            location: "US",
+            favShows: [
+                "something",
+                "something",
+                "something"
+            ]
+        },
+
+        {
+            name: 'Katie',
+            location: "US",
+            favShows: [
+                "something",
+                "something",
+                "something"
+            ]
+        },
+
+        {
+            name: 'Ron',
+            location: "US",
+            favShows: [
+                "something",
+                "something",
+                "something"
+            ]
+        }
+    ]
+}
+
+console.log(data);
