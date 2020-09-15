88554c98a60751a938b1936b6ac1a35cfaa21bac
min <minhazur9@gmail.com>
2020-09-15 19:25:12 -0400

    data structures

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+// Data Types
+
 // Light Switch would be a boolean
 // Email would be a string
 // Spaceship would be an object
@@ -5,56 +7,79 @@
 // Each having a location would be an array of objects 
 // Each having a list of shows would be an array of objects containing another array
 
-const data = {
-    switchOn: false,
-    email: "minhazur9@gmail.com",
-    spaceship: {
-        hull: true,
-        laserBlaster: true,
-        tractorBeam: true,
-        warpDrive: true
-    },
-    students: [
-        {
-            name: "Min",
-            location: "US",
-            favShows: [
-                "Dexter",
-                "Gotham",
-                "Brookyn Nine Nine"
-            ]
-        },
+// const data = {
+//     switchOn: false,
+//     email: "minhazur9@gmail.com",
+//     spaceship: {
+//         hull: true,
+//         laserBlaster: true,
+//         tractorBeam: true,
+//         warpDrive: true
+//     },
+//     students: [
+//         {
+//             name: "Min",
+//             location: "US",
+//             favShows: [
+//                 "Dexter",
+//                 "Gotham",
+//                 "Brookyn Nine Nine"
+//             ]
+//         },
 
-        {
-            name: "William",
-            location: "US",
-            favShows: [
-                "something",
-                "something",
-                "something"
-            ]
-        },
+//         {
+//             name: "William",
+//             location: "US",
+//             favShows: [
+//                 "something",
+//                 "something",
+//                 "something"
+//             ]
+//         },
 
-        {
-            name: 'Katie',
-            location: "US",
-            favShows: [
-                "something",
-                "something",
-                "something"
-            ]
-        },
+//         {
+//             name: 'Katie',
+//             location: "US",
+//             favShows: [
+//                 "something",
+//                 "something",
+//                 "something"
+//             ]
+//         },
 
-        {
-            name: 'Ron',
-            location: "US",
-            favShows: [
-                "something",
-                "something",
-                "something"
-            ]
-        }
-    ]
-}
+//         {
+//             name: 'Ron',
+//             location: "US",
+//             favShows: [
+//                 "something",
+//                 "something",
+//                 "something"
+//             ]
+//         }
+//     ]
+// }
 
-console.log(data);
+// console.log(data);
+
+// Take it Easy
+
+rainbow = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'indigo',
+    'blue',
+    'violet',
+]
+
+rainbow[5];
+
+const Min = {
+    favFood: "Steak",
+    hobby: "Video Games",
+    town: "Atlantic City",
+    dataType: "Number"
+};  
+
+Min.hobby;
