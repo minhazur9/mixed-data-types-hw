560245640d2c4b68d764ac35173a7973e5b12a38
min <minhazur9@gmail.com>
2020-09-16 01:03:59 -0400

    uncommented code

--- scripts/app.js
+++ scripts/app.js
@@ -7,166 +7,166 @@
 // Each having a location would be an array of objects 
 // Each having a list of shows would be an array of objects containing another array
 
-// const data = {
-//     switchOn: false,
-//     email: "minhazur9@gmail.com",
-//     spaceship: {
-//         hull: true,
-//         laserBlaster: true,
-//         tractorBeam: true,
-//         warpDrive: true
-//     },
-//     students: [
-//         {
-//             name: "Min",
-//             location: "US",
-//             favShows: [
-//                 "Dexter",
-//                 "Gotham",
-//                 "Brookyn Nine Nine"
-//             ]
-//         },
+const data = {
+    switchOn: false,
+    email: "minhazur9@gmail.com",
+    spaceship: {
+        hull: true,
+        laserBlaster: true,
+        tractorBeam: true,
+        warpDrive: true
+    },
+    students: [
+        {
+            name: "Min",
+            location: "US",
+            favShows: [
+                "Dexter",
+                "Gotham",
+                "Brookyn Nine Nine"
+            ]
+        },
 
-//         {
-//             name: "William",
-//             location: "US",
-//             favShows: [
-//                 "something",
-//                 "something",
-//                 "something"
-//             ]
-//         },
+        {
+            name: "William",
+            location: "US",
+            favShows: [
+                "something",
+                "something",
+                "something"
+            ]
+        },
 
-//         {
-//             name: 'Katie',
-//             location: "US",
-//             favShows: [
-//                 "something",
-//                 "something",
-//                 "something"
-//             ]
-//         },
+        {
+            name: 'Katie',
+            location: "US",
+            favShows: [
+                "something",
+                "something",
+                "something"
+            ]
+        },
 
-//         {
-//             name: 'Ron',
-//             location: "US",
-//             favShows: [
-//                 "something",
-//                 "something",
-//                 "something"
-//             ]
-//         }
-//     ]
-// }
+        {
+            name: 'Ron',
+            location: "US",
+            favShows: [
+                "something",
+                "something",
+                "something"
+            ]
+        }
+    ]
+}
 
-// console.log(data);
+console.log(data);
 
 // Take it Easy
 
-// rainbow = [
-//     'red',
-//     'orange',
-//     'yellow',
-//     'green',
-//     'indigo',
-//     'blue',
-//     'violet',
-// ]
+rainbow = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'indigo',
+    'blue',
+    'violet',
+]
 
-// rainbow[5];
+rainbow[5];
 
-// const Min = {
-//     favFood: "Steak",
-//     hobby: "Video Games",
-//     town: "Atlantic City",
-//     dataType: "Number"
-// };  
+const Min = {
+    favFood: "Steak",
+    hobby: "Video Games",
+    town: "Atlantic City",
+    dataType: "Number"
+};  
 
-// Min.hobby;
+Min.hobby;
 
 // Crazy Object
 
-// const crazyObject = {
-//     taco: [
-//       {
-//         meat: 'steak',
-//         cheese: ['panela', 'queso', 'chihuahua']
-//       },
-//       {
-//         meat: 'chicken',
-//         salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
-//       },
-//     ],
-//     larry: {
-//       nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-//       quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-//       characters: [
-//         {
-//           name: "Jeff",
-//           occupation: "manager"
-//         },
-//         {
-//           name: "funkhauser",
-//           occupation: "tv dude"
-//         },
-//         {
-//           name: "susie",
-//           occupation: "jeffs wife",
-//           favourtieHobby: "Swearing at Larry and Jeff"
-//         },
-//       ]
-//     }
-//   }
+const crazyObject = {
+    taco: [
+      {
+        meat: 'steak',
+        cheese: ['panela', 'queso', 'chihuahua']
+      },
+      {
+        meat: 'chicken',
+        salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+      },
+    ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [
+        {
+          name: "Jeff",
+          occupation: "manager"
+        },
+        {
+          name: "funkhauser",
+          occupation: "tv dude"
+        },
+        {
+          name: "susie",
+          occupation: "jeffs wife",
+          favourtieHobby: "Swearing at Larry and Jeff"
+        },
+      ]
+    }
+  }
 
-//   console.log(crazyObject.taco[1].salsa[5]);
-//   console.log(crazyObject.larry.quotes[0]);
-//   console.log(crazyObject.larry.characters[2].favourtieHobby);
-//   console.log(crazyObject.larry.nicknames[1]);
+  console.log(crazyObject.taco[1].salsa[5]);
+  console.log(crazyObject.larry.quotes[0]);
+  console.log(crazyObject.larry.characters[2].favourtieHobby);
+  console.log(crazyObject.larry.nicknames[1]);
 
-//   function findName(name) {
-//   for(const property in crazyObject) {
-//       if(property === "larry"){
-//           const larryObj = crazyObject[property];
-//           for(const larryProp in larryObj){
-//               if(larryProp === "characters"){
-//                   const larryChar = larryObj[larryProp];
-//                   for(let i = 0; i < larryChar.length; i++){
-//                       if(larryChar[i].name === name){
-//                           return larryChar[i];
-//                       }
-//                   }
-//               }
-//           }
+  function findName(name) {
+  for(const property in crazyObject) {
+      if(property === "larry"){
+          const larryObj = crazyObject[property];
+          for(const larryProp in larryObj){
+              if(larryProp === "characters"){
+                  const larryChar = larryObj[larryProp];
+                  for(let i = 0; i < larryChar.length; i++){
+                      if(larryChar[i].name === name){
+                          return larryChar[i];
+                      }
+                  }
+              }
+          }
           
-//       }
-//   }
-// }
+      }
+  }
+}
 
-// console.log(findName("funkhauser"));
+console.log(findName("funkhauser"));
 
-// Object-ception
+Object-ception
 
-// const inception = {
-//   reality: {
-//       dreamLayer1: {
-//           dreamLayer2: {
-//               dreamLayer3: {
-//                   dreamLayer4: {
-//                       dreamLayer5: {
-//                           dreamLayer6: {
-//                               limbo: "Joseph Gordon Levitt"
-//                          }
-//                       }
-//                   }
-//               }
-//           }
-//       }
-//   }
-// }
+const inception = {
+  reality: {
+      dreamLayer1: {
+          dreamLayer2: {
+              dreamLayer3: {
+                  dreamLayer4: {
+                      dreamLayer5: {
+                          dreamLayer6: {
+                              limbo: "Joseph Gordon Levitt"
+                         }
+                      }
+                  }
+              }
+          }
+      }
+  }
+}
 
-// inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
 
-// console.log(inception);
+console.log(inception);
 
 // Bond Films
 
@@ -196,11 +196,11 @@ const bondFilms = [
   { "title" : "License to Kill", "year" : 1989, "actor" : "Timothy Dalton", "gross" : "$285,157,191" }
 ];
 
-// const bondTitles = [];
-// for(let i = 0; i < bondFilms.length; i++) {
-//   bondTitles[i] = bondFilms[i].title
-// }
-// console.log(bondTitles);
+const bondTitles = [];
+for(let i = 0; i < bondFilms.length; i++) {
+  bondTitles[i] = bondFilms[i].title
+}
+console.log(bondTitles);
 
 let gross = 0;
 for(let i = 0; i < bondFilms.length; i++) {
