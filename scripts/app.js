2e2364bb6acbac46f125863d71448413b3700c1c
min <minhazur9@gmail.com>
2020-09-15 20:09:41 -0400

    crazyObject

--- scripts/app.js
+++ scripts/app.js
@@ -63,23 +63,83 @@
 
 // Take it Easy
 
-rainbow = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'indigo',
-    'blue',
-    'violet',
-]
+// rainbow = [
+//     'red',
+//     'orange',
+//     'yellow',
+//     'green',
+//     'indigo',
+//     'blue',
+//     'violet',
+// ]
 
-rainbow[5];
+// rainbow[5];
 
-const Min = {
-    favFood: "Steak",
-    hobby: "Video Games",
-    town: "Atlantic City",
-    dataType: "Number"
-};  
+// const Min = {
+//     favFood: "Steak",
+//     hobby: "Video Games",
+//     town: "Atlantic City",
+//     dataType: "Number"
+// };  
 
-Min.hobby;
+// Min.hobby;
+
+// Crazy Object
+
+const crazyObject = {
+    taco: [
+      {
+        meat: 'steak',
+        cheese: ['panela', 'queso', 'chihuahua']
+      },
+      {
+        meat: 'chicken',
+        salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+      },
+    ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [
+        {
+          name: "Jeff",
+          occupation: "manager"
+        },
+        {
+          name: "funkhauser",
+          occupation: "tv dude"
+        },
+        {
+          name: "susie",
+          occupation: "jeffs wife",
+          favourtieHobby: "Swearing at Larry and Jeff"
+        },
+      ]
+    }
+  }
+
+  console.log(crazyObject.taco[1].salsa[5]);
+  console.log(crazyObject.larry.quotes[0]);
+  console.log(crazyObject.larry.characters[2].favourtieHobby);
+  console.log(crazyObject.larry.nicknames[1]);
+
+  function findName(name) {
+  for(const property in crazyObject) {
+      if(property === "larry"){
+          const larryObj = crazyObject[property];
+          for(const larryProp in larryObj){
+              if(larryProp === "characters"){
+                  const larryChar = larryObj[larryProp];
+                  for(let i = 0; i < larryChar.length; i++){
+                      if(larryChar[i].name === name){
+                          return larryChar[i];
+                      }
+                  }
+              }
+          }
+          
+      }
+  }
+}
+
+  console.log(findName("funkhauser"));
